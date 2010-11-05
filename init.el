93d76087738b00ba6b35b8a459de5d371e327a06
Gunther Diemant <g.diemant@gmx.net>
2010-11-05 13:59:43 +0100

    init.el now loads all files in config dir.

---
+++ init.el
@@ -0,0 +1,21 @@
+ ;;;;;;;;;;;;;;;
+;;; Constants ;;;
+ ;;;;;;;;;;;;;;;
+
+(defconst gd-emacs-dir "~/.emacs.d/")
+(defconst gd-config-dir (concat gd-emacs-dir "config"))
+(defconst gd-pkg-dir (concat gd-emacs-dir "pkg"))
+
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Adjusting emacs load path ;;;
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'load-path gd-config-dir)
+(add-to-list 'load-path gd-pkg-dir)
+
+
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Loading my configuration files ;;;
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(dolist (configfile (split-string (shell-command-to-string (concat "ls" gd-config-dir))))
+  (load (replace-regexp-in-string "\\.el" "" configfile)))
