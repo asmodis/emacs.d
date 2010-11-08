c892f6fcc12ef5081bd3421ccf5e25f0951e209d
Gunther Diemant <g.diemant@gmx.net>
2010-11-08 12:14:05 +0100

    Added encoding settings.

--- config/gd-base-config.el
+++ config/gd-base-config.el
@@ -17,6 +17,19 @@
   delete-old-versions t)
 
 
+ ;;;;;;;;;;;;;;
+;;; Encoding ;;;
+ ;;;;;;;;;;;;;;
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")       ; prefer utf-8 for language settings
+(set-input-method nil)                   ; no funky input for normal editing;
+(setq read-quoted-char-radix 10)         ; use decimal, not octal
+
+
 ;;; Require stuff which is needed all the time
 (require 'cl)
 
