a9d021c2146d68918cd2be520ffd49b11c66f345
Gunther Diemant <g.diemant@gmx.net>
2010-11-08 12:27:40 +0100

    Moved some configurations into the proper file.

--- config/gd-base-config.el
+++ config/gd-base-config.el
@@ -33,22 +33,6 @@
 ;;; Require stuff which is needed all the time
 (require 'cl)
 
-;;; Disable unneeded things
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message nil)
-
-
-;;; Turn on some usful things
-(show-paren-mode t)
-(setq show-paren-style "mixed")
-(setq query-replace-highlight t)
-(setq search-highlight t)
-(transient-mark-mode t)
-(setq font-lock-maximum-decoration t)
-(global-font-lock-mode t)
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq require-final-newline t)
 
--- config/gd-look-config.el
+++ config/gd-look-config.el
@@ -1,5 +1,9 @@
 (provide 'gd-look-config)
 
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; General appereance of a frame ;;;
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
 (add-to-list 'default-frame-alist '(height . 32))
 (add-to-list 'default-frame-alist '(width . 90))
@@ -9,3 +13,24 @@
 (add-to-list 'default-frame-alist '(cursor-color . "gray70"))
 (add-to-list 'default-frame-alist '(mouse-color . "gray70"))
 (add-to-list 'default-frame-alist '(cursor-type . hbar))
+
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+
+ ;;;;;;;;;;
+;;; Misc ;;;
+ ;;;;;;;;;;
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+
+(show-paren-mode t)
+(setq show-paren-style "mixed")
+
+(setq query-replace-highlight t)
+(setq search-highlight t)
+(transient-mark-mode t)
+
+(setq font-lock-maximum-decoration t)
+(global-font-lock-mode t)
