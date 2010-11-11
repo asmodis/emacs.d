f068110f6724bb1be4f577ce4dbd9753b00f6689
Gunther Diemant <g.diemant@gmx.net>
2010-11-11 10:43:57 +0100

    Fixed loading of config files.
    It seems it is no good idea to use shell-command at startup...

--- init.el
+++ init.el
@@ -14,9 +14,8 @@
 (add-to-list 'load-path gd-config-dir)
 (add-to-list 'load-path gd-pkg-dir)
 
-
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Loading my configuration files ;;;
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(dolist (configfile (split-string (shell-command-to-string (concat "ls" gd-config-dir))))
-  (load (replace-regexp-in-string "\\.el" "" configfile)))
+(dolist (configfile (directory-files gd-config-dir nil ".*\\.el"))
+   (load (replace-regexp-in-string "\\.el" "" configfile)))
