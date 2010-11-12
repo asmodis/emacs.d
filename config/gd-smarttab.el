8783dfb602f77026244a57a38f310f8cf963e035
Gunther Diemant <g.diemant@gmx.net>
2010-11-12 10:23:29 +0100

    Let samrttab using hippie-expand.
    Better integration with yasnippet.

--- config/gd-smarttab.el
+++ config/gd-smarttab.el
@@ -1,4 +1,6 @@
 (provide 'gd-smarttab)
 
 (require 'smart-tab)
+(setq smart-tab-using-hippie-expand t)
+(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
 (global-smart-tab-mode 1)
