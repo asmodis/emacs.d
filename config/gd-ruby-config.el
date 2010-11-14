a3517b1747020c4db325717e4175db420acec2be
Gunther Diemant <g.diemant@gmx.net>
2010-11-14 18:11:05 +0100

    Fixed a missing '

--- config/gd-ruby-config.el
+++ config/gd-ruby-config.el
@@ -3,7 +3,7 @@
 ;;Associating files with ruby
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec\\" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
 
 
 ;;Ruby mode hook
